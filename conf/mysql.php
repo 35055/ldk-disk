641e99ce4a6f2f7bf7dc12d956642d4359277ca4
350553002@qq.com <350553002@qq.com>
2018-03-13 17:27:50 +0800

    add

---
+++ conf/mysql.php
@@ -0,0 +1,21 @@
+<?php return array (
+  'master' => 
+  array (
+    'user' => 'root',
+    'pwd' => 'root',
+    'host' => '127.0.0.1',
+    'port' => 3306,
+    'dbname' => 'disk',
+  ),
+  'slave' => 
+  array (
+    0 => 
+    array (
+      'user' => 'root',
+      'pwd' => 'root',
+      'host' => '127.0.0.1',
+      'port' => 3306,
+      'dbname' => 'disk',
+    ),
+  ),
+); ?>
